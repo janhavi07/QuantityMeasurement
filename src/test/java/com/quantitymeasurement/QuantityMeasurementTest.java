f29ece3d861a0d4454fbf2b3885352eacf9be0d6
Suraj7852 <surajpj7852@gmail.com>
2019-12-17 13:19:13 +0530

    Welcome to Quantity Measurement

---
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -0,0 +1,11 @@
+package com.quantitymeasurement;
+
+import org.junit.Test;
+
+public class QuantityMeasurementTest {
+
+    @Test
+    public void given0FeetAnd0Feet_ShouldReturnTrue() {
+
+    }
+}
